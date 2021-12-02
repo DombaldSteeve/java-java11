2ecab9ebd3ab1dba948d819fa3eef0cd55ef5b72
Steeve <steeve.dombald.pro@outlook.com>
2021-12-02 12:49:38 +0100

    lambda_01_corrigé sans is()

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -1,19 +1,14 @@
 package java8.ex01;
 
-import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
-import java.util.ArrayList;
-import java.util.List;
+import java8.data.Data;
+import java8.data.Person;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.Test;
 
-import java8.data.Data;
-import java8.data.Person;
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Exercice 01 - Filter
@@ -28,13 +23,12 @@ public class Lambda_01_Test {
 
     // tag::filter[]
     private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
-        // TODO implementer la méthode
-    	List<Person> filteredPersons = new ArrayList<>();
+        List<Person> filteredPersons = new ArrayList<Person>();
     	for (Person p: persons){
-    		if (predicate.test(p)){
-    			filteredPersons.add(p);
-    		}
-    	}
+        	if (predicate.test(p)){
+        		filteredPersons.add(p);
+        	}
+        }
         return filteredPersons;
     }
     // end::filter[]
@@ -47,10 +41,13 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, p->p.getAge()>=18);
+        List<Person> result = filter(personList, p -> p.getAge() >= 18);
 
-        assertThat(result.size(), is(83));
-        assertThat(result, everyItem(hasProperty("age", greaterThan(17))));
+        assert result.size() == 83;
+
+        for (Person person : result) {
+            assert person.getAge() > 17;
+        }
     }
     // end::test_filter_by_age[]
 
@@ -61,10 +58,11 @@ public class Lambda_01_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, p->p.getFirstname().equals("first_10"));
+        List<Person> result = filter(personList, p -> p.getFirstname().equals("first_10"));
 
-        assertThat(result.size(), is(1));
-        assertThat(result, everyItem(hasProperty("firstname", is("first_10"))));
+        assert result.size() == 1;
+        assert result.get(0).getFirstname().equals("first_10");
+
     }
     // end::test_filter_by_firstname[]
 
@@ -80,8 +78,10 @@ public class Lambda_01_Test {
         // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
         List<Person> result = filter(personList, p->p.getAge()>49 && DigestUtils.sha512Hex(p.getPassword()).equals(passwordSha512Hex));
 
-        assertThat(result.size(), is(6));
-        assertThat(result, everyItem(hasProperty("password", is("test"))));
+        assert result.size() == 6;
+        for (Person person : result) {
+            assert person.getPassword().equals("test");
+        }
     }
     // end::test_filter_by_password[]
 }
